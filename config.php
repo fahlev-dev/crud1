29afd591f1d00e5d56b00aabda1815ecd78c9308
Reza Fahlevi <rfahlevi501@gmail.com>
2023-01-12 16:49:45 +0700

    commit pertama aplikasi crud1

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php 
+
+$databaseHost = 'localhost';
+$databaseName = 'crud_db';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$conn = mysqli_connect($databaseHost,$databaseUsername,$databasePassword,$databaseName);
+
+?>
